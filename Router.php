b4d300120b6ae460bd6cef2b38979493933c5897
Julio Cesar Choque Mamani <122507585+JCChoqueM@users.noreply.github.com>
2025-09-21 22:26:42 -0400

    iniciando proyecto

---
+++ Router.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace MVC;
+
+class Router
+{
+
+    public $rutasGET = [];
+    public $rutasPOST = [];
+
+    public function get($url, $fn)
+    {
+
+        $this->rutasGET[$url] = $fn;
+    }
+    public function post($url, $fn)
+    {
+        $this->rutasPOST[$url] = $fn;
+    }
+
+    public function comprobarRutas()
+    {
+        session_start();
+        $auth = $_SESSION['login'] ?? null;
+
+        //Arreglo de rutas protegidas
+        $rutasProtegidas = ['/admin', '/propiedades/crear', '/propiedades/actualizar', '/propiedades/eliminar', '/vendedores/crear', '/vendedores/actualizar', '/vendedores/eliminar'];
+
+        $urlActual = strtok($_SERVER['REQUEST_URI'], '?') ?? '/'; /*  $urlActual = $_SERVER['PATH_INFO'] ?? '/'; */
+        $metodo = $_SERVER['REQUEST_METHOD'];
+
+        if ($metodo === 'GET') {;
+            $fn = $this->rutasGET[$urlActual] ?? null;
+        } else {
+
+            $fn = $this->rutasPOST[$urlActual] ?? null;
+        }
+
+        //Proteger rutas
+        if (in_array($urlActual, $rutasProtegidas) && !$auth) {
+            header('Location: /');
+        }
+
+        if ($fn) {
+            //La url existe y hay una fn asociada  
+
+            call_user_func($fn, $this);
+        } else {
+
+            echo "Pagina no encontrada";
+        }
+    }
+
+    //Muestra una vista
+    public function render($view, $datos = [])
+    {
+        foreach ($datos as $key => $value) {
+
+
+            $$key = $value;
+        }
+        ob_start();
+        include __DIR__ . "/views/$view.php";
+        $contenido = ob_get_clean();
+
+        include __DIR__ . "/views/layout.php";
+    }
+}
