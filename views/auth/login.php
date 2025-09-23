ed66df1e4c946f75c006ba6314b0b6eabada0c40
Julio Cesar Choque Mamani <122507585+JCChoqueM@users.noreply.github.com>
2025-09-22 22:43:34 -0400

    449 creando el fomulario login

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,1 +1,27 @@
-<h1>Login nada mas</h1>
+<h1 class="nombre-pagina">Login</h1>
+<p class="descripcion-pagina">Inicia sesión con tus datos</p>
+
+<form class="formulario" method="post" action="/">
+    <div class="campo">
+        <label for="email">Email</label>
+        <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Tu Email">
+    </div>
+    <div class="password">
+        <label for="password">Password</label>
+        <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Tu Password">
+    </div>
+    <input type="submit" class="boton" value="Iniciar Sesión">
+
+</form>
+<div class="acciones">
+    <a href="/crear-cuenta">¿?Aun no tienes una cuenta? Crear una</a>
+    <a href="/olvide">¿Olvidaste tu Password?</a>
+</div>
