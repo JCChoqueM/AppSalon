807f00d584afefa501b59b7916c10e2f04378312
Julio Cesar Choque Mamani <122507585+JCChoqueM@users.noreply.github.com>
2025-10-13 00:16:06 -0400

    487 - Anadiendo los Contenedores Principales

--- views/cita/index.php
+++ views/cita/index.php
@@ -1,2 +1,23 @@
 <h1 class="nombre-pagina">Crear Nueva Cita</h1>
 <p class="descripcion-pagina">Elige tus servicios y coloca tus datos</p>
+<div id="app">
+    <div id="paso-1" class="seccion">
+        <h2>Servicios</h2>
+        <p>Elije tus servicios a continuacion</p>
+        <div id="servicios" class="listado-servicios">
+
+        </div>
+    </div>
+    <div id="paso-2" class="seccion">
+        <h2>Servicios</h2>
+        <p>Coloca tus datos y fecha de tu cita</p>
+
+
+    </div>
+    <div id="paso-3" class="seccion">
+        <h2>resumen</h2>
+        <p>verficia que la informacion sea correcta</p>
+
+    </div>
+
+</div>
