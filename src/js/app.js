02e187c5799472a838ea76b7ba433aa9d0cba5ca
Julio Cesar Choque Mamani <122507585+JCChoqueM@users.noreply.github.com>
2025-10-29 23:36:39 -0400

    494 - Finalizando la navegacion por tabs

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function iniciarApp() {
+  mostrarSeccion(); // muestra y oculta las secciones
   tabs(); /* Cambia la seccion cuando se presionen los tabs */
 }
 function mostrarSeccion() {
@@ -17,7 +18,18 @@ function mostrarSeccion() {
   const pasoSelector = `#paso-${paso}`;
   const seccion = document.querySelector(pasoSelector);
   seccion.classList.add('mostrar');
+
+  //Quita la clase de actual al tab anterior
+  const tabAnterior = document.querySelector('.actual');
+  if (tabAnterior) {
+    tabAnterior.classList.remove('actual');
+  }
+
+  //Resalta el tab actual
+  const tab = document.querySelector(`[data-paso="${paso}"]`);
+  tab.classList.add('actual');
 }
+
 function tabs() {
   const botones = document.querySelectorAll('.tabs button');
   botones.forEach((boton) => {
