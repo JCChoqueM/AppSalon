0a72a731c3f02dee380be0618fe53aa7a52c55aa
Julio Cesar Choque Mamani <122507585+JCChoqueM@users.noreply.github.com>
2025-10-30 22:16:31 -0400

    498 - Creando el Modelo de Servicios y exportando a JSON

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 require_once __DIR__ . '/../includes/app.php';
 
 use Controllers\CitaController;
+use Controllers\APIController;
 use Controllers\LoginController;
 use MVC\Router;
 
@@ -29,6 +30,10 @@ $router->get("/mensaje", [LoginController::class, "mensaje"]);
 
 //Area Privada
 $router->get("/cita", [CitaController::class, "index"]);
+
+//API de Citas
+$router->get("/api/servicios", [APIController::class, "index"]);
+
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas(); 
