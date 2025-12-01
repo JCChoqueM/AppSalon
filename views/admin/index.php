f243d1eb8d3127f16a367c8e14014cca67e03c5e
Julio Cesar Choque Mamani <122507585+JCChoqueM@users.noreply.github.com>
2025-12-01 11:49:12 -0400

    css a los datos mostrados

--- views/admin/index.php
+++ views/admin/index.php
@@ -19,6 +19,34 @@ include_once __DIR__ . '/../templates/barra.php';
 </div>
 
 <div id="citas-admin">
+    <ul class="citas">
+
+        <?php
+
+        $idCita = 0; //para que no marque undefined
+        foreach ($citas as $cita) { //inicio foreach
+
+            if ($idCita !== $cita->id) { // inicio if
+        ?>
+                <li>
+                    <p> ID: <span> <?php echo $cita->id; ?> </span></p>
+                    <p> Hora: <span> <?php echo $cita->hora; ?> </span></p>
+                    <p> Cliente: <span> <?php echo $cita->cliente; ?> </span></p>
+                    <p> Email: <span> <?php echo $cita->email; ?> </span></p>
+                    <p> Telefono: <span> <?php echo $cita->telefono; ?> </span></p>
+
+                    <h3>Servicios</h3>
+                <?php
+                $idCita = $cita->id;
+            } // fin if 
+                ?>
+
+                <p class="servicio"> <?php echo $cita->servicio; ?> </p>
+                <?php
+        } //fin foreach
+        ?>
+        </li>
 
 
+    </ul>
 </div>
